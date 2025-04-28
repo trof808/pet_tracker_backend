40a127301e723be9dc885e3369cfc6c198f62214
n.trofimov <n.trofimov@MacBook-Pro-hhuser.local>
2025-04-28 15:34:34 +0300

    fix auth

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from app.auth.security import SECRET_KEY, ALGORITHM
@@ -6,12 +6,15 @@ from app.auth.security import SECRET_KEY, ALGORITHM
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="sign_in")
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+def get_current_user(request: Request):
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -19,7 +22,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         email: str = payload.get("sub")
         id = payload.get("id")
         if email is None:
-            raise credentials_exception
+            raise HTTPException(status_code=401, detail="Invalid token")
         return {"email": email, "id": id}  # Возвращаем информацию о пользователе
     except:
-        raise credentials_exception
+        raise HTTPException(status_code=401, detail="Invalid token")
